4810d0eed02ad2ceb869058e330d922a232efbbd
woshiren <yejiacong@outlook.com>
2023-08-11 19:38:06 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -36,43 +36,45 @@ int main(int argc,char** argv)
         return -1; 
     }
 
-    ros::Rate loop_rate(100);
-    while (ros::ok())
-    {
-        ros::spinOnce();
-        geometry_msgs::PoseStamped base_pose;
-        geometry_msgs::TransformStamped base_in_world;
-        tf2_ros::Buffer tfBuffer;
-        tf2_ros::TransformListener listener(tfBuffer);
-        try
-        {
-            base_in_world=tfBuffer.lookupTransform("base_link","map",ros::Time(0),ros::Duration(0.2));
-        }
-        catch(const tf2::TransformException &e)
-        {
-            ROS_ERROR("%s",e.what());
-            continue;
-        }
-        base_pose.header=base_in_world.header;
-        base_pose.pose.position.x=base_in_world.transform.translation.x;
-        base_pose.pose.position.y=base_in_world.transform.translation.y;
-        base_pose.pose.position.z=base_in_world.transform.translation.z;
-        base_pose.pose.orientation=base_in_world.transform.rotation;
+    ros::spin();
 
-        tf2::Quaternion q(
-        base_pose.pose.orientation.x,
-        base_pose.pose.orientation.y,
-        base_pose.pose.orientation.z,
-        base_pose.pose.orientation.w);
-        tf2::Matrix3x3 m(q);
-        double roll, pitch, yaw;
-        m.getRPY(roll, pitch, yaw);
+    // ros::Rate loop_rate(100);
+    // while (ros::ok())
+    // {
+    //     ros::spinOnce();
+    //     geometry_msgs::PoseStamped base_pose;
+    //     geometry_msgs::TransformStamped base_in_world;
+    //     tf2_ros::Buffer tfBuffer;
+    //     tf2_ros::TransformListener listener(tfBuffer);
+    //     try
+    //     {
+    //         base_in_world=tfBuffer.lookupTransform("base_link","map",ros::Time(0),ros::Duration(0.2));
+    //     }
+    //     catch(const tf2::TransformException &e)
+    //     {
+    //         ROS_ERROR("%s",e.what());
+    //         continue;
+    //     }
+    //     base_pose.header=base_in_world.header;
+    //     base_pose.pose.position.x=base_in_world.transform.translation.x;
+    //     base_pose.pose.position.y=base_in_world.transform.translation.y;
+    //     base_pose.pose.position.z=base_in_world.transform.translation.z;
+    //     base_pose.pose.orientation=base_in_world.transform.rotation;
 
-        ROS_INFO("x: %f y: %f yaw: %f",base_pose.pose.position.x,base_pose.pose.position.y,yaw);
-        geometry_msgs::Pose::ConstPtr basePosePtr = boost::make_shared<const geometry_msgs::Pose>(base_pose.pose);
-        myChassis.update_chassis_posture(basePosePtr);
-        // myChassis.exec();
-        loop_rate.sleep();
-    }
+    //     tf2::Quaternion q(
+    //     base_pose.pose.orientation.x,
+    //     base_pose.pose.orientation.y,
+    //     base_pose.pose.orientation.z,
+    //     base_pose.pose.orientation.w);
+    //     tf2::Matrix3x3 m(q);
+    //     double roll, pitch, yaw;
+    //     m.getRPY(roll, pitch, yaw);
+
+    //     ROS_INFO("x: %f y: %f yaw: %f",base_pose.pose.position.x,base_pose.pose.position.y,yaw);
+    //     geometry_msgs::Pose::ConstPtr basePosePtr = boost::make_shared<const geometry_msgs::Pose>(base_pose.pose);
+    //     myChassis.update_chassis_posture(basePosePtr);
+    //     // myChassis.exec();
+    //     loop_rate.sleep();
+    // }
     return 0;
 }
